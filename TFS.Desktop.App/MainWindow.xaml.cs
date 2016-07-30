bddf047e8eec4fe94ec05060209d20174afd387b
Imanuel Ulbricht <i.ulbricht@codinglizards.de>
2016-07-30 17:34:43 +0200

    Added basic windows UI

---
+++ TFS.Desktop.App/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.TeamFoundation.Client;
+using System.Windows;
+using TFS.Desktop.App.ViewModels;
+
+namespace TFS.Desktop.App {
+
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private async void MetroWindow_Loaded(object sender, RoutedEventArgs e) {
+            this.DataContext = await MainWindowViewModel.GetViewModel();
+        }
+    }
+}
---
+++ TFS.Desktop.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace TFS.Desktop.App.ViewModels {
+    using System.Threading.Tasks;
+    using Utils;
+
+    public class MainWindowViewModel : NotifyPropertyChanged {
+        private string greeting;
+
+        public string Greeting {
+            get { return $"Hello {greeting}!"; }
+            set {
+                if (greeting != value) {
+                    greeting = value;
+                    OnPropertyChanged("Greeting");
+                }
+            }
+        }
+
+        public static async Task<MainWindowViewModel> GetViewModel() {
+            return new MainWindowViewModel();
+        }
+    }
+}
